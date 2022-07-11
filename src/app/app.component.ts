b0a1462beaeedf505dcc0500c1f2200ea2075ac2
subhomoychakrabarti <subhomoy.chakrabarti@gmail.com>
2022-07-11 10:14:47 +0530

    Chap 21 reached

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,20 @@ export class AppComponent {
   title = 'simpleCRM';
 
   success_msg = false;
+
+
+  contacts=[
+    {
+    'firstname':'ABC',
+   'lastname' :'tutorials',
+   'contactsId':1234
+    }, {
+      'firstname': 'DEF',
+      'lastname': 'tutorials',
+      'contactsId': 5432
+    }
+]
+
+ superhero = 'Batm'
+ styleProp = 'red' //string write in double quote
 }
